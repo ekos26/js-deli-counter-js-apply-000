5d1f458e21dc3c12a5d663f855bdea13a8e3bd01
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2018-12-31 21:11:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,38 @@ function nowServing(line) {
 function currentLine(line) {
   const arr = [];
   
-  if (!line.length) {
-    return 'The line is currently empty.';
+  if (line.length === 0) {
+    return "The line is currently empty.";
   } else {
-    for (let i = 0; i < line.length; i++){
-      arr.push(`${i + 1}. ${line[i]}`)
+    for (let i = 0; i < line.length; i++) {
+      arr.push(`${i+1}. ${line[i]}`);
     }
+    // line.forEach(function(element, i) {
+    //   arr.push(`${i + 1}. ${element}`)
+    // })
     return `The line is currently: ${arr.join(', ')}`;
   }
 }
+
+
+
+
+var ticketNumber = 1;
+
+function takeANumber(line){
+  line.push(ticketNumber);
+  return 'Welcome! You\'re ticket number #' + ticketNumber++;
+  
+}
+
+takeANumber(line) // "Welcome! You're ticket number #1" //[1]
+takeANumber(line) // "Welcome! You're ticket number #2" [1,2]
+
+nowServing(line) // "Currently serving #1" [2]
+nowServing(line) // "Currently serivng #2" []
+
+takeANumber(line) // "Welcome! You're ticket number #3" [3]
+
+
+
+
